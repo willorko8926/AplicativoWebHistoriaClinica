5463118f810263878d4825b9dc66fee1ab607439
JohnnyCalderon <johnnycalderon27@gmail.com>
2018-01-28 23:03:21 -0500

    pag de adinistracion

--- administrador.php
+++ administrador.php
@@ -1,10 +1,10 @@
 <?php
-$nombreusuario = $_POST['nombreusuario'];
+#$nombreusuario = $_POST['nombreusuario'];
 ?>
 <!DOCTYPE html>
 <html lang="es">
   <head>
-    <link rel="icon" type="image/png" href="../img/logo-nave.ico">
+    <link rel="icon" type="image/png" href="img/logo-nave.ico">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Medikal-HCE</title>
@@ -13,31 +13,31 @@ $nombreusuario = $_POST['nombreusuario'];
     <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 
     <!-- CSS -->
-    <link rel="stylesheet" href="../css/owl.carousel.css">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/font-awesome.min.css">
-    <link rel="stylesheet" href="../css/styleapp.css">
-    <link rel="stylesheet" href="../css/ionicons.min.css">
-    <link rel="stylesheet" href="../css/animate.css">
-    <link rel="stylesheet" href="../css/responsive.css">
-    <link rel="stylesheet" href="../css/examlab.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/styleapp.css">
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/examlab.css">
 
     <!-- Js -->
     <script src="../js/vendor/modernizr-2.6.2.min.js"></script>
     <script src="..///ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/owl.carousel.min.js"></script>
-    <script src="../js/plugins.js"></script>
-    <script src="../js/min/waypoints.min.js"></script>
-    <script src="../js/jquery.counterup.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/min/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.js"></script>
 
     <!-- Google Map -->
     <script src="https://maps.googleapis.com/maps/api/js"></script>
-    <script src="../js/google-map-init.js"></script>
+    <script src="js/google-map-init.js"></script>
 
 
-    <script src="../js/main.js"></script>
+    <script src="js/main.js"></script>
 
 
   </head>
@@ -50,13 +50,13 @@ $nombreusuario = $_POST['nombreusuario'];
             <div class="row">
               <div class="col-md-6 col-sm-6">
                 <div class="block">
-                  <h1>Perfil de Usuario</h1>
+                  <h1>Perfil de Administrador</h1>
                 </div>
               </div>
               <div class="col-md-6 col-sm-6">
                 <div class="block">
                   
-                  <h5 style="text-align: right; margin-right:20px"><a href="../index.php" >SALIR  </h5>
+                  <h5 style="text-align: right; margin-right:20px"><a href="index.php" >SALIR  </h5>
 
                 </div>
               </div>
@@ -74,77 +74,39 @@ $nombreusuario = $_POST['nombreusuario'];
                   <div class="row">
                     <div class="col-md-12 col-sm-12">
 
-                      <h4 class="underline">Información del Usuario</h4>
+                      <h4 class="underline">Información del Administrador</h4>
+                    </div>
+                  </div>
+             <div class="row">
+                    <div class="col-md-2 col-sm-2">
+                      <label class="form-label">Administrador:</label>
+                    </div>
+                    <div class="col-md-4 col-sm-4">
+                      <input type="text" class="form-control" placeholder=" Nombre">
                     </div>
                   </div>
                   <div class="row">
                     <div class="col-md-2 col-sm-2">
-                      <label class="form-label"> Nombre del Paciente:</label>
+                      <label class="form-label">Codigo:</label>
                     </div>
                     <div class="col-md-4 col-sm-4">
-                      <label class="form-label"> Johnny Calderon</label>
-                    </div>
-
-                        <div class="col-md-2 col-sm-2">
-                      <label class="form-label"> Correo Electronico:</label>
-                    </div>
-                    <div class="col-md-4 col-sm-4">
-                      <label class="form-label"> johnny_calderon94@gmail.com</label>
-                    </div>
-
-                        <div class="col-md-2 col-sm-2">
-                      <label class="form-label"> ID: </label>
-                    </div>
-                    <div class="col-md-4 col-sm-4">
-                      <label class="form-label"> 0912345678 </label>
-                    </div>
-
-                               <div class="col-md-2 col-sm-2">
-                      <label class="form-label"> Sexo: </label>
-                    </div>
-                    <div class="col-md-4 col-sm-4">
-                      <label class="form-label"> Masculino</label>
+                      <input type="text" class="form-control" placeholder="xxxxxxxx">
                     </div>
                   </div>
-
 
                   <div class="row"><p></p></div>
                   <div class="row">
                     <div class="col-md-12 col-sm-12">
-                      <h4 class="underline">Servicios </h4>
+                      <h4 class="underline">Tablas </h4>
                     </div>
-                  </div>
-
+                  </div>  
                   <div class="row">
                     <div class="col-md-2 col-sm-2">
-                       <div class="image">
-                        <a href="HCE-FichaUsuario.php"><img src="../img/Ficha.redimensionado.png" alt="ficha"/></a>
+                        <div class="cta">
+                          <a href="paginas/registro/registrarse.php">Registrarse </a>
                         </div>
                     </div>
-                    <div class="col-md-2 col-sm-2">
-                         <div class="image">
-                           <a href="HCE-HistoriaResum.php"><img src="../img/Resumido%20HC.redimensionado.png" alt="Resumido"/></a>
-                         </div>
-                     </div>
-                     <div class="col-md-2 col-sm-2">
-                          <div class="image">
-                             <a href="HCE-ConsultaExterna.php"><img src="../img/consultaEx2.redimensionado.png" alt="Consulta"/></a>
-                          </div>
-                     </div>
-                       <div class="col-md-2 col-sm-2">
-                        <div class="image">
-                         <a href="HTE-Emergencias.php"><img src="../img/Urgencia.redimensionado.png" alt="Urgencia"/></a>
-                        </div>
-                    </div>
-                    <div class="col-md-2 col-sm-2">
-                        <div class="image">
-                           <a href="HCE-Laboratorio.php"><img src="../img/ExLab.redimensionado.png" alt="Examenes"/></a>
-                          </div>
-                    </div>
-
                   </div>
-
-
 
                 </div>
               </form>

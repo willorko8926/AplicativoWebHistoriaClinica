696a9c3c87738864faf68facdb15bc492fa4e197
OliverDiaz10 <odiazno@hotmail.com>
2018-02-13 23:38:24 -0500

    Incorporación de Funcionalidad a Estándar de Páginas de la Aplicación

--- src/admin/registrarse.php
+++ src/admin/registrarse.php
@@ -1,50 +1,133 @@
 <?php
-  session_start();
+session_start();
+include_once($_SERVER['DOCUMENT_ROOT'] . "/src/lib/pllGeneral.php");
+
+if (mVerificaSesion() != 1){
+    header('location:fault.php');
+}
+$pUserName = $_SESSION['SSUserName'];
 ?>
+<!DOCTYPE html>
 <html lang="es">
 <head>
-<style>
-    table, td, th {
-        border: 1px solid black;
-    }
-    table {
-        border-collapse: collapse;
-        width: 80%;
-    }
-    th, td{
-        text-align: left;
-        padding: 5px;
-    }
-</style>
+    <link rel="icon" type="image/png" href="../img/logo-nave.ico">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Medikal-HCE</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../css/owl.carousel.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../css/styleapp.css">
+    <link rel="stylesheet" href="../../css/ionicons.min.css">
+    <link rel="stylesheet" href="../../css/animate.css">
+    <link rel="stylesheet" href="../../css/responsive.css">
+    <link rel="stylesheet" href="../../css/examlab.css">
+    
+    <!-- Js -->
+    <script src="../js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="..///ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/plugins.js"></script>
+    <script src="../js/min/waypoints.min.js"></script>
+    <script src="../js/jquery.counterup.js"></script>
+
+    <!-- Google Map -->
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="../js/google-map-init.js"></script>
+    <script src="../js/main.js"></script>
 </head>
 <body>
-    <header>
-    </header>
-    <h2>Administración de Registros de Usuarios</h2>
-    <table>
-    <tr>
-        <th>Num.Reg.</th>
-        <th>Identificación</th>
-        <th>Nombres</th>
-        <th>Apellidos</th>
-        <th><a href="registrarseCreate.php">Crear</a></th>
-    </tr>
-<?php
-    include_once("CRegUsuarioCollector.php");
-    $id = 1;
-    $lobRegUsuarioCollector = new CRegUsuarioCollector();
-    foreach ($lobRegUsuarioCollector->selectALL() as $r){
-        echo "<tr>";
-        echo "    <td class='col_left_10'>".$r->getNumeroRegistro()."</td>";
-        echo "    <td class='col_left_20'>".$r->getNumeroIdentificacion()."</td>";
-        echo "    <td class='col_left_40'>".$r->getNombres()."</td>";
-        echo "    <td class='col_left_10'>".$r->getApellidos()."</td>";
-        echo "    <td><a href='registrarseEdit.php?id=".$r->getNumeroRegistro()."'>Editar</a> <a href='registrarseDML.php?id=".$r->getNumeroRegistro()."&tipoOperacion=3'>Eliminar</a></td>";
-        echo "</tr>";
-    }
-?>
-    </table>
-    <br>
-    <a href="../../login.php">Volver Login</a>    
+    <!-- Slider Start -->
+    <section id="global-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-sm-6">
+            <div class="block">
+              <h1>MedikalHCE©</h1>
+              <h1>Administración del Sistema</h1>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="block">
+              <div><?php echo "<h5 style='text-align: right; color:white'>Usuario: ".$pUserName."</h5>"; ?></div>
+              <div></div>
+              <div style="text-align: right; padding_right:10px"><a href="../logout.php" style="color:white">Cerrar Sesión</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Administracion Start -->
+    <section id="examen-form">
+      <div class="container">
+        <div class="block">
+          <div class="row">
+            <div class="col-md-12 col-sm-12">
+              <h4 class="underline">Registro de Usuario</h4>
+            </div>
+          </div>
+
+<!--************************************************************************************-->
+<!--Consulta de Datos de la Tabla-->
+<!--Cabecera de la Tabla-->
+          <form action="../../admin.php" method="POST">
+              <table id="examen-list">
+                <tr>
+                  <th class="col-center">Num.Reg.</th>
+                  <th class="col-left">Identificación</th>
+                  <th class="col-left">Nombres</th>
+                  <th class="col-left">Apellidos</th>
+                  <th class="col-center"><a href="registrarseCreate.php" style="color:white">Crear</a></th>
+                </tr>
+<!--************************************************************************************-->
+<!--Detalle de la Tabla-->
+            <?php
+              include_once("CRegUsuarioCollector.php");
+              $id = 1;
+              $lobRegUsuarioCollector = new CRegUsuarioCollector();
+              foreach ($lobRegUsuarioCollector->selectALL() as $r){
+                echo "<tr>";
+                echo "    <td class='col-center'>".$r->getNumeroRegistro()."</td>";
+                echo "    <td class='col-left'>".$r->getNumeroIdentificacion()."</td>";
+                echo "    <td class='col-left'>".$r->getNombres()."</td>";
+                echo "    <td class='col-left'>".$r->getApellidos()."</td>";
+                echo "    <td class='col-center'><a href='registrarseEdit.php?id=".$r->getNumeroRegistro()."'>Editar</a>   <a href='registrarseDML.php?id=".$r->getNumeroRegistro()."&tipoOperacion=3'>Eliminar</a></td>";
+                echo "</tr>";
+              }
+            ?>
+              </table>
+<!--************************************************************************************-->
+
+              <br><br><br>
+              <div class="row"><p></p></div>
+              <div class="row">
+                <div class="col-md-9 col-sm-9"></div>
+                <div class="col-md-2 col-sm-2">
+                  <button class="btn btn-app">Menú Administración</button>
+                </div>
+              </div>
+          </form>
+        </div>
+      </div><br><br><br><br><br>
+    </section>
+
+<!-- Call to action Start -->
+    <section id="global-header">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4"></div>
+            <div class="col-md-4 col-sm-4"><p style="color:white">Derechos Reservados MedikalHCE©</p></div>
+            <div class="col-md-4 col-sm-4"></div>
+        </div>
+      </div>
+    </section>
 </body>
 </html>
--- src/admin/registrarseEdit.php
+++ src/admin/registrarseEdit.php
@@ -1,50 +1,192 @@
 <?php
-  session_start();
+session_start();
+include_once($_SERVER['DOCUMENT_ROOT'] . "/src/lib/pllGeneral.php");
+
+if (mVerificaSesion() != 1){
+    header('location:fault.php');
+}
+$pUserName = $_SESSION['SSUserName'];
 ?>
+<!DOCTYPE html>
 <html lang="es">
 <head>
+    <link rel="icon" type="image/png" href="../img/logo-nave.ico">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Medikal-HCE</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../css/owl.carousel.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../css/styleapp.css">
+    <link rel="stylesheet" href="../../css/ionicons.min.css">
+    <link rel="stylesheet" href="../../css/animate.css">
+    <link rel="stylesheet" href="../../css/responsive.css">
+    <link rel="stylesheet" href="../../css/examlab.css">
+    
+    <!-- Js -->
+    <script src="../js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="..///ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/plugins.js"></script>
+    <script src="../js/min/waypoints.min.js"></script>
+    <script src="../js/jquery.counterup.js"></script>
+
+    <!-- Google Map -->
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="../js/google-map-init.js"></script>
+    <script src="../js/main.js"></script>
 </head>
 <body>
-    <header>
-    </header> 
-    <h2>Modificación de Registros de Usuario</h2>
-    
-    <?php
-        include_once("CRegUsuarioCollector.php");
-        include_once("CMgRegistroUsuario.php");
-        
-        $id = $_GET["id"];
-        $lobRegUsuarioCollector = new CRegUsuarioCollector();
-        $lobRegUsuario = $lobRegUsuarioCollector->selectPK($id);
-    ?>
-    
-    <form action="registrarseDML.php?tipoOperacion=2" method="post">
-    <fieldset>
-        <label>Núm. Registro: </label>
-        <input type="text" name="txtNumeroRegistro" value="<?php echo $lobRegUsuario->getNumeroRegistro(); ?>" readonly><br><br>
-        <label>Núm. Identificación: </label>
-        <input type="text" name="txtIdentificacion" value="<?php echo $lobRegUsuario->getNumeroIdentificacion(); ?>"><br><br>
-        <label>Nombres: </label>
-        <input type="text" name="txtNombres" value="<?php echo $lobRegUsuario->getNombres(); ?>"><br><br>
-        <label>Apellidos: </label>
-        <input type="text" name="txtApellidos" value="<?php echo $lobRegUsuario->getApellidos(); ?>"><br><br>
-        <label>Sexo: </label>
-        <input type="text" name="txtSexo" value="<?php echo $lobRegUsuario->getSexo(); ?>"><br><br>
-        <label>Nacionalidad: </label>
-        <input type="text" name="txtNacionalidad" value="<?php echo $lobRegUsuario->getNacionalidad(); ?>"><br><br>
-        <label>Dirección: </label>
-        <input type="text" name="txtDireccion" value="<?php echo $lobRegUsuario->getDireccion(); ?>"><br><br>
-        <label>Celular: </label>
-        <input type="text" name="txtCelular" value="<?php echo $lobRegUsuario->getCelular(); ?>"><br><br>
-        <label>Correo Electrónico: </label>
-        <input type="text" name="txtCorreo" value="<?php echo $lobRegUsuario->getCorreoElectronico(); ?>"><br><br>
-        <label>País de Residencia: </label>
-        <input type="text" name="txtCodigoPais" value="<?php echo $lobRegUsuario->getCodigoPaisResidencia(); ?>"><br>
-    </fieldset>
-    <fieldset>
-        <button type="submit" style="padding:5px">Actualizar</button> <a href="registrarse.php">Cancelar</a>
-        <br>
-    </fieldset>
-    </form>
+    <!-- Slider Start -->
+    <section id="global-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-sm-6">
+            <div class="block">
+              <h1>MedikalHCE©</h1>
+              <h1>Administración del Sistema</h1>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="block">
+              <div><?php echo "<h5 style='text-align: right; color:white'>Usuario: ".$pUserName."</h5>"; ?></div>
+              <div></div>
+              <div style="text-align: right; padding_right:10px"><a href="../logout.php" style="color:white">Cerrar Sesión</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Administracion Start -->
+    <section id="examen-form">
+      <div class="container">
+        <div class="block">
+          <div class="row">
+            <div class="col-md-12 col-sm-12">
+              <h4 class="underline">Modificar Registro de Usuario</h4>
+            </div>
+          </div>
+
+<!--************************************************************************************-->
+<!--Consulta de Datos de la Tabla-->
+<!--Cabecera de la Tabla-->
+          <?php
+            include_once("CRegUsuarioCollector.php");
+            include_once("CMgRegistroUsuario.php");
+
+            $id = $_GET["id"];
+            $lobRegUsuarioCollector = new CRegUsuarioCollector();
+            $lobRegUsuario = $lobRegUsuarioCollector->selectPK($id);
+          ?>
+          <form action="registrarseDML.php?tipoOperacion=2" method="post">
+            <div class="row">
+              <div class="col-md-2 col-sm-2">
+                <label>Núm. Registro: </label>
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <input type="text" name="txtNumeroRegistro" value="<?php echo $lobRegUsuario->getNumeroRegistro(); ?>" readonly><br>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-2 col-sm-2">
+                <label>Núm. Identificación: </label>
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <input type="text" name="txtIdentificacion" value="<?php echo $lobRegUsuario->getNumeroIdentificacion(); ?>"><br>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-2 col-sm-2">
+                <label>Nombres: </label>
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <input type="text" name="txtNombres" value="<?php echo $lobRegUsuario->getNombres(); ?>"><br>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-2 col-sm-2">
+                <label>Apellidos: </label>
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <input type="text" name="txtApellidos" value="<?php echo $lobRegUsuario->getApellidos(); ?>"><br>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-2 col-sm-2">
+                <label>Sexo: </label>
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <input type="text" name="txtSexo" value="<?php echo $lobRegUsuario->getSexo(); ?>"><br>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-2 col-sm-2">
+                <label>Nacionalidad: </label>
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <input type="text" name="txtNacionalidad" value="<?php echo $lobRegUsuario->getNacionalidad(); ?>"><br>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-2 col-sm-2">
+                <label>Dirección: </label>
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <input type="text" name="txtDireccion" value="<?php echo $lobRegUsuario->getDireccion(); ?>"><br>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-2 col-sm-2">
+                <label>Celular: </label>
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <input type="text" name="txtCelular" value="<?php echo $lobRegUsuario->getCelular(); ?>"><br>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-2 col-sm-2">
+                <label>Correo Electrónico: </label>
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <input type="text" name="txtCorreo" value="<?php echo $lobRegUsuario->getCorreoElectronico(); ?>"><br>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-2 col-sm-2">
+                <label>País de Residencia: </label>
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <input type="text" name="txtCodigoPais" value="<?php echo $lobRegUsuario->getCodigoPaisResidencia(); ?>"><br><br>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-5 col-sm-5">
+                <button type="submit" class="btn btn-app">Actualizar</button> <a href="registrarse.php"> Cancelar </a><br>
+              </div>
+            </div>
+          </form>
+<!--************************************************************************************-->
+        </div>
+      </div><br><br>
+    </section>
+
+<!-- Call to action Start -->
+    <section id="global-header">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4"></div>
+            <div class="col-md-4 col-sm-4"><p style="color:white">Derechos Reservados MedikalHCE©</p></div>
+            <div class="col-md-4 col-sm-4"></div>
+        </div>
+      </div>
+    </section>
 </body>
 </html>

e9704390c174dc422338332cc49a99308667cfd1
OliverDiaz10 <odiazno@hotmail.com>
2018-02-13 23:35:32 -0500

    Creación de Menú de Administración y Logout del Sistema

---
+++ admin.php
@@ -0,0 +1,136 @@
+<?php
+session_start();
+include_once($_SERVER['DOCUMENT_ROOT'] . "/src/lib/pllGeneral.php");
+
+if (mVerificaSesion() != 1){
+    header('location:fault.php');
+}
+$pUserName = $_SESSION['SSUserName'];
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="icon" type="image/png" href="../img/logo-nave.ico">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Medikal-HCE</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../css/owl.carousel.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/styleapp.css">
+    <link rel="stylesheet" href="../css/ionicons.min.css">
+    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+
+    <!-- Js -->
+    <script src="../js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="..///ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/plugins.js"></script>
+    <script src="../js/min/waypoints.min.js"></script>
+    <script src="../js/jquery.counterup.js"></script>
+
+    <!-- Google Map -->
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="../js/google-map-init.js"></script>
+    <script src="../js/main.js"></script>
+</head>
+<body>
+    <!-- Slider Start -->
+    <section id="global-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-sm-6">
+            <div class="block">
+              <h1>MedikalHCE©</h1>
+              <h1>Administración del Sistema</h1>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="block">
+              <div><?php echo "<h5 style='text-align: right; color:white'>Usuario: ".$pUserName."</h5>"; ?></div>
+              <div></div>
+              <div style="text-align: right; padding_right:10px"><a href="../logout.php" style="color:white">Cerrar Sesión</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Administracion Start -->
+    <section id="registrarse-form">
+      <div class="container">
+        <div class="block">
+          <form action="admin/HCE-Main.php" method="POST">
+            <div class="form-group">
+              <div class="row">
+                <div class="col-md-6 col-sm-6">
+                  <h4 class="underline">Tablas Módulo General</h4>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                  <h4 class="underline">Tablas Módulo Seguridad</h4>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6 col-sm-6">
+                  <div><a href="">Países</a></div>
+                  <div><a href="">Contactos</a></div>
+                  <div><a href="src/admin/registrarse.php">Registro de Usuarios</a></div>
+                  <div><a href="src/admin/persona.php">Personas</a></div>
+                  <div><a href="src/admin/personaContacto.php">Personas / Contactos</a></div>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                  <div><a href="">Roles</a></div>
+                  <div><a href="">Usuarios</a></div>
+                  <div><a href="">Roles x Usuario</a></div>
+                </div>
+              </div>
+              <br>
+              <div class="row">
+                <div class="col-md-6 col-sm-6">
+                  <h4 class="underline">Tablas Historia Clínica</h4>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6 col-sm-6">
+                  <div><a href="">Instituciones</a></div>
+                  <div><a href="">Médicos</a></div>
+                  <div><a href="">Resumen Clínico</a></div>
+                  <div><a href="">Atención de Consultas</a></div>
+                  <div><a href="">Atención Emergencias</a></div>
+                  <div><a href="src/admin/examen.php">Exámenes de Laboratorio</a></div>
+                </div>
+              </div>
+
+              <div class="row"><p></p></div>
+              <div class="row">
+                <div class="col-md-10 col-sm-10"></div>
+                <div class="col-md-2 col-sm-2">
+                  <button class="btn btn-app">Menú Principal</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div><br><br><br><br><br>
+    </section>
+
+<!-- Call to action Start -->
+    <section id="global-header">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4"></div>
+            <div class="col-md-4 col-sm-4"><p style="color:white">Derechos Reservados MedikalHCE©</p></div>
+            <div class="col-md-4 col-sm-4"></div>
+        </div>
+      </div>
+    </section>
+</body>
+</html>
---
+++ logout.php
@@ -0,0 +1,152 @@
+<?php
+session_start();
+$_SESSION = array();
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html >
+<head>
+  <link rel="icon" type="image/png" href="img/logo-nave.ico">
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Medikal-HCE</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+    <!-- Js -->
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/min/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.js"></script>
+
+    <!-- Google Map -->
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="js/google-map-init.js"></script>
+
+
+    <script src="js/main.js"></script>
+
+<!-- ******************* -->
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="free-educational-responsive-web-template-webEdu">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
+<link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+<link rel="stylesheet" href="css/styleLog.css">
+<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen">
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen">
+	<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<!-- Header Start -->
+<header>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <!-- header Nav Start -->
+          <nav class="navbar navbar-default">
+            <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">
+                  <img src="img/LOGE.png" alt="Logo">
+                </a>
+              </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="somos.php">Nosotros</a></li>
+                    <li><a href="servicios.php">Servicio</a></li>
+                    <li><a href="contact.php">Contacto</a></li>
+                    <li><a href="login.php">Iniciar Sesión</a></li>
+                  </ul>
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>
+          </div>
+        </div>
+      </div>
+</header><!-- header close -->
+<body>
+    <div class="pen-title">
+      <h1>MEDIKAL-HCE</h1>
+    </div>
+
+    <!-- Form Module-->
+    <div class="module form-module">
+      <div class="toggle">
+      </div>
+
+      <div class="form">
+        <h2>Salir del Sistema</h2>
+        <br>
+        <h4>Gracias por Usar Nuestros Servicios.</h4>
+        <form id='filldetails'  method='POST' action='login.php'>
+          <fielset>
+          <button>Volver a Login</button>
+          </fielset>
+        </form>
+      </div>
+    </div><br><br><br>
+
+<!-- footer Start -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="footer-manu">
+              <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="somos.php">Nosotros</a></li>
+                <li><a href="servicios.php">Servicio</a></li>
+                <li><a href="contact.php">Contacto</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+	<!-- Google Maps -->
+	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+	<script src="assets/js/google-map.js"></script>
+
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src="js/indexLog.js"></script>
+</body>
+</html>

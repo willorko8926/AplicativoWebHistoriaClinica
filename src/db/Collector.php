2d167978335f25a8e1f7f069b75bbb4593867b91
OliverDiaz10 <odiazno@hotmail.com>
2018-02-16 06:39:23 -0500

    Configuración de Acceso Base de Datos en Heroku

--- src/db/Collector.php
+++ src/db/Collector.php
@@ -2,10 +2,10 @@
 include_once('DataBase.php');
 
 // Define configuration
-define("DB_HOST", "localhost");
-define("DB_USER", "postgres");
-define("DB_PASS", "postgres");
-define("DB_NAME", "HCE");
+define("DB_HOST", "ec2-54-163-236-188.compute-1.amazonaws.com");
+define("DB_USER", "ppeahbtfrwsikw");
+define("DB_PASS", "92778e3c4f13f322d77a3c129e52204ed6f6c0013cd6b4f6ae333ac37d62c660");
+define("DB_NAME", "d4mv4chcffaldq");
 
 class Collector extends DataBase
 {

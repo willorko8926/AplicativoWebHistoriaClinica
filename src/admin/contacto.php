4864ee9aad482bf5eecd8b56fa33609be9bd7313
RosyChamba <rchamba@espol.edu.ec>
2018-02-16 00:50:13 -0500

    Aplicación de Estandar de pag en contacto

--- src/admin/contacto.php
+++ src/admin/contacto.php
@@ -7,52 +7,129 @@ if (mVerificaSesion() != 1){
 }
 $pUserName = $_SESSION['SSUserName'];
 ?>
+<!DOCTYPE html>
 <html lang="es">
 <head>
-<style>
-    table, td, th {
-        border: 1px solid black;
-    }
-    table {
-        border-collapse: collapse;
-        width: 80%;
-    }
-    th, td{
-        text-align: left;
-        padding: 5px;
-    }
-</style>
+    <link rel="icon" type="image/png" href="../img/logo-nave.ico">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Medikal-HCE</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../css/owl.carousel.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../css/styleapp.css">
+    <link rel="stylesheet" href="../../css/ionicons.min.css">
+    <link rel="stylesheet" href="../../css/animate.css">
+    <link rel="stylesheet" href="../../css/responsive.css">
+    <link rel="stylesheet" href="../../css/examlab.css">
+    
+    <!-- Js -->
+    <script src="../js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="..///ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/plugins.js"></script>
+    <script src="../js/min/waypoints.min.js"></script>
+    <script src="../js/jquery.counterup.js"></script>
+
+    <!-- Google Map -->
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="../js/google-map-init.js"></script>
+    <script src="../js/main.js"></script>
 </head>
 <body>
-    <header>
-    </header>
-    <h2>Administración de Contacto</h2>
-    <table>
-    <tr>
-        <th>Num. Contacto</th>
-        <th>Nombres</th>
-        <th>Correo electrónico</th>
-        <th>Asunto</th>
-        <th>Mensaje</th>
-        <th><a href="contactoCreate.php">Crear Contacto</a></th>
+<!-- Slider Start -->
+    <section id="global-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-sm-6">
+            <div class="block">
+              <h1>MedikalHCE©</h1>
+              <h1>Administración del Sistema</h1>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="block">
+              <div><?php echo "<h5 style='text-align: right; color:white'>Usuario: ".$pUserName."</h5>"; ?></div>
+              <div></div>
+              <div style="text-align: right; padding_right:10px"><a href="../../logout.php" style="color:white">Cerrar Sesión</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Administracion Start -->
+    <section id="examen-form">
+      <div class="container">
+        <div class="block">
+          <div class="row">
+            <div class="col-md-12 col-sm-12">
+              <h4 class="underline">Administración de Contacto</h4>
+            </div>
+          </div>
+
+<!--************************************************************************************-->
+<!--Consulta de Datos de la Tabla-->
+<!--Cabecera de la Tabla-->
+         <form action="../../admin.php" method="POST">
+              <table id="examen-list"> 
+                <tr>
+                    <th class="col-center">Num. Contacto</th>
+                    <th class="col-left">Nombres</th>
+                    <th class="col-left">Correo electrónico</th>
+                    <th class="col-left">Asunto</th>
+                    <th class="col-left">Mensaje</th>
+                    <th class="col-center"><a href="contactoCreate.php" style="color:white">CREAR CONTACTO</a></th>
     </tr>
+<!--************************************************************************************-->
+<!--Detalle de la Tabla-->
 <?php
     include_once("CContactoCollector.php");
     $id = 1;
     $lobContactoCollector = new CContactoCollector();
     foreach ($lobContactoCollector->selectALL() as $r){
         echo "<tr>";  //preguntar si hay que poner el pk o id aquí
-        echo "    <td class='col_left_10'>".$r->getNumeroContacto()."</td>";
-        echo "    <td class='col_left_10'>".$r->getNombres()."</td>";
-        echo "    <td class='col_left_20'>".$r->getCorreoElectronico()."</td>";
-        echo "    <td class='col_left_40'>".$r->getAsunto()."</td>";
-        echo "    <td class='col_left_10'>".$r->getMensaje()."</td>";
-        echo "    <td><a href='contactoEdit.php?id=".$r->getNumeroContacto()."'>Editar</a> <a href='contactoDML.php?id=".$r->getNumeroContacto()."&tipoOperacion=3'>Eliminar</a></td>";
+        echo "    <td class='col-center'>".$r->getNumeroContacto()."</td>";
+        echo "    <td class='col-left'>".$r->getNombres()."</td>";
+        echo "    <td class='col-left'>".$r->getCorreoElectronico()."</td>";
+        echo "    <td class='col-left'>".$r->getAsunto()."</td>";
+        echo "    <td class='col-left'>".$r->getMensaje()."</td>";
+        echo "    <td class='col-center'><a href='contactoEdit.php?id=".$r->getNumeroContacto()."'>Editar</a> <a href='contactoDML.php?id=".$r->getNumeroContacto()."&tipoOperacion=3'>Eliminar</a></td>";
         echo "</tr>";
     }
 ?>
     </table>
-    <br>
-    <a href="../../login.php">Volver Login</a>    
+<!--************************************************************************************-->
+
+              <br><br><br>
+              <div class="row"><p></p></div>
+              <div class="row">
+                <div class="col-md-9 col-sm-9"></div>
+                <div class="col-md-2 col-sm-2">
+                  <button class="btn btn-app">Menú Administración</button>
+                </div>
+              </div>
+          </form>
+        </div>
+      </div><br><br><br><br><br>
+    </section>
+
+<!-- Call to action Start -->
+    <section id="global-header">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4"></div>
+            <div class="col-md-4 col-sm-4"><p style="color:white">Derechos Reservados MedikalHCE©</p></div>
+            <div class="col-md-4 col-sm-4"></div>
+        </div>
+      </div>
+    </section>
 </body>
 </html>

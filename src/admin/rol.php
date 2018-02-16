a93fc9cb0bd3c5ee4b6871956abdaefd23ac514b
RosyChamba <rchamba@espol.edu.ec>
2018-02-16 00:51:55 -0500

    Aplicación de Estandar de pag en rol

--- src/admin/rol.php
+++ src/admin/rol.php
@@ -7,46 +7,122 @@ if (mVerificaSesion() != 1){
 }
 $pUserName = $_SESSION['SSUserName'];
 ?>
+<!DOCTYPE html>
 <html lang="es">
 <head>
-<style>
-    table, td, th {
-        border: 1px solid black;
-    }
-    table {
-        border-collapse: collapse;
-        width: 80%;
-    }
-    th, td{
-        text-align: left;
-        padding: 5px;
-    }
-</style>
+    <link rel="icon" type="image/png" href="../img/logo-nave.ico">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Medikal-HCE</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="../../css/owl.carousel.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../../css/styleapp.css">
+    <link rel="stylesheet" href="../../css/ionicons.min.css">
+    <link rel="stylesheet" href="../../css/animate.css">
+    <link rel="stylesheet" href="../../css/responsive.css">
+    <link rel="stylesheet" href="../../css/examlab.css">
+    
+    <!-- Js -->
+    <script src="../js/vendor/modernizr-2.6.2.min.js"></script>
+    <script src="..///ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/owl.carousel.min.js"></script>
+    <script src="../js/plugins.js"></script>
+    <script src="../js/min/waypoints.min.js"></script>
+    <script src="../js/jquery.counterup.js"></script>
+
+    <!-- Google Map -->
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="../js/google-map-init.js"></script>
+    <script src="../js/main.js"></script>
 </head>
 <body>
-    <header>
-    </header>
-    <h2>Administración de Roles</h2>
-    <table>
-    <tr>
-        <th>Código Rol</th>
-        <th>Descripción</th>
-        <th><a href="rolCreate.php">Crear nuevo Rol</a></th>
+        <!-- Slider Start -->
+    <section id="global-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-sm-6">
+            <div class="block">
+              <h1>MedikalHCE©</h1>
+              <h1>Administración del Sistema</h1>
+            </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="block">
+              <div><?php echo "<h5 style='text-align: right; color:white'>Usuario: ".$pUserName."</h5>"; ?></div>
+              <div></div>
+              <div style="text-align: right; padding_right:10px"><a href="../../logout.php" style="color:white">Cerrar Sesión</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Administracion Start -->
+    <section id="examen-form">
+      <div class="container">
+        <div class="block">
+          <div class="row">
+            <div class="col-md-12 col-sm-12">
+              <h4 class="underline">Administración de Roles</h4>
+            </div>
+          </div>
+<!--************************************************************************************-->
+<!--Consulta de Datos de la Tabla-->
+<!--Cabecera de la Tabla-->
+          <form action="../../admin.php" method="POST">
+              <table id="examen-list">
+                <tr>
+                    <th class="col-center">Código Rol</th>
+                    <th class="col-left">Descripción</th>
+                    <th class="col-center"><a href="rolCreate.php" style="color:white">CREAR ROL</a></th>
     </tr>
+<!--************************************************************************************-->
+<!--Detalle de la Tabla-->
 <?php
     include_once("CRolCollector.php");
     $id = 1;
     $lobRolCollector = new CRolCollector();
     foreach ($lobRolCollector->selectALL() as $r){
-        echo "<tr>";  //preguntar si hay que poner el pk o id aquí
-        echo "    <td class='col_left_10'>".$r->getCodigoRol()."</td>";
-        echo "    <td class='col_left_10'>".$r->getDescripcion()."</td>";
-        echo "    <td><a href='rolEdit.php?id=".$r->getCodigoRol()."'>Editar</a> <a href='rolDML.php?id=".$r->getCodigoRol()."&tipoOperacion=3'>Eliminar</a></td>";
+        echo "<tr>";
+        echo "    <td class='col-center'>".$r->getCodigoRol()."</td>";
+        echo "    <td class='col-left'>".$r->getDescripcion()."</td>";
+        echo "    <td class='col-center'><a href='rolEdit.php?id=".$r->getCodigoRol()."'>Editar</a> <a href='rolDML.php?id=".$r->getCodigoRol()."&tipoOperacion=3'>Eliminar</a></td>";
         echo "</tr>";
     }
 ?>
     </table>
-    <br>
-    <a href="../../login.php">Volver Login</a>    
+<!--************************************************************************************-->
+
+              <br><br><br>
+              <div class="row"><p></p></div>
+              <div class="row">
+                <div class="col-md-9 col-sm-9"></div>
+                <div class="col-md-2 col-sm-2">
+                  <button class="btn btn-app">Menú Administración</button>
+                </div>
+              </div>
+          </form>
+        </div>
+      </div><br><br><br><br><br>
+    </section>
+
+<!-- Call to action Start -->
+    <section id="global-header">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4"></div>
+            <div class="col-md-4 col-sm-4"><p style="color:white">Derechos Reservados MedikalHCE©</p></div>
+            <div class="col-md-4 col-sm-4"></div>
+        </div>
+      </div>
+    </section>
 </body>
 </html>
